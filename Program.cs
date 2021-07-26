97f3ba3e59e0749f0289d7c90c1dbaaf0fa632c2
real-ge3k <61907962+real-ge3k@users.noreply.github.com>
2021-07-26 23:13:21 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversor_de_moeda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Repeat play = new Repeat();
+            play.conversor();
+           
+            Console.WriteLine("Quer repetir? (S/N)");
+            string resp = Console.ReadLine();
+            if (resp == "S" || resp == "s")
+            {
+                Console.Clear();
+                Repeat repeat = new Repeat();
+                repeat.conversor();
+            }
+            else if (resp == "N" || resp == "n")
+                Console.WriteLine("OBRIGADO POR USAR O NOSSO PROGRAMA.");
+            else
+                Console.WriteLine("[ERRO] -  Verique o dado de entrada.");
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ Repeat.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversor_de_moeda
+{
+    class Repeat
+    {
+        public void conversor()
+        {
+            const float câmbio = 0.0016f;
+            // Partindo do pressuposto que um kwanza angolano equivale à 0,0016 dollar americano 
+            // Dados de entrada: valor
+            // Processamento: dolarAmericano = valor * cambioAtual
+            // Saida: dolarAmericano
+
+            Console.WriteLine("------------------------------------------------------\t");
+            Console.WriteLine("------------------------------------------------------\t");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\n               [KWANZA PARA DOLLAR] \n");
+            Console.ResetColor();
+            Console.WriteLine("------------------------------------------------------\t");
+            Console.WriteLine("------------------------------------------------------\t");
+            try
+            {
+                Console.Write("\nInsira o valor.\n");
+                decimal valor = decimal.Parse(Console.ReadLine());
+                decimal dollar = valor * Convert.ToDecimal(câmbio);
+                Console.BackgroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("Equivalente a Dollar: ${0}", dollar);
+                Console.ResetColor();
+                Console.WriteLine("\nDados de moeda fornecidos pela Morningstar e de criptomoeda fornecidos pela coinbase.");
+            }
+            catch(Exception ee)
+            {
+                Console.WriteLine(ee.Message);
+            }
+        }
+    }
+}
